db709d758cf3223f11d9acb27566d61be79b93e0
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-06-15 09:58:35 -0600

    help from james

--- src/App.js
+++ src/App.js
@@ -30,18 +30,41 @@ const keyBoardArr = [keysRow1, keysRow2, keysRow3];
 function App() {
   const [wordleGuessList, setWordleGuessList] = useState([...defaultGuessList]);
   const [arrCoords, setArrayCoords] = useState([0, 0]);
+
+  const handleKeyEvent = (e) => {
+    const newGuess = e.target.id;
+    console.log(newGuess);
+    // const updatedWordleGuessList = [...wordleGuessList];
+    const updatedWordleGuessList = [
+      [...wordleGuessList[0]],
+      [...wordleGuessList[1]],
+      [...wordleGuessList[2]],
+      [...wordleGuessList[3]],
+      [...wordleGuessList[4]],
+      [...wordleGuessList[5]],
+    ];
+
+    const rowCoord = arrCoords[0];
+    const colCoord = arrCoords[1];
+    const squareIndex = handleDelete(newGuess, colCoord);
+
+    const newWordleRow = updatedWordleGuessList[rowCoord];
+    newWordleRow[squareIndex] = setKeyValue(newGuess, squareIndex);
+
+    const newIndex = setIndexValue(newGuess, squareIndex);
+    const newArrCoords = [...arrCoords];
+    newArrCoords[1] = newIndex;
+    setArrayCoords(newArrCoords);
+    setWordleGuessList(updatedWordleGuessList);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="Cool">Wordle Copy</h1>
         <div>Answer: {answer}</div>
         <ColumnComponent wordleGuessList={wordleGuessList} />
-        <KeyBoardComponent
-          wordleGuessList={wordleGuessList}
-          setWordleGuessList={setWordleGuessList}
-          arrCoords={arrCoords}
-          setArrayCoords={setArrayCoords}
-        />
+        <KeyBoardComponent handleKeyEvent={handleKeyEvent} />
       </header>
     </div>
   );
@@ -55,7 +78,12 @@ const RowComponent = (props) => {
   return (
     <div className="Wordle-row">
       {props.row.map((square, index) => {
-        return <SquareComponent key={index} square={square}></SquareComponent>;
+        return (
+          <SquareComponent
+            key={`square-component-${props.rowIndex}-${index}`}
+            square={square}
+          ></SquareComponent>
+        );
       })}
     </div>
   );
@@ -65,59 +93,57 @@ const ColumnComponent = (props) => {
   return (
     <div className="Wordle-column">
       {props.wordleGuessList.map((row, index) => {
-        return <RowComponent key={index} row={row}></RowComponent>;
+        return (
+          <RowComponent
+            key={`row-component-${index}`}
+            rowIndex={index}
+            row={row}
+          ></RowComponent>
+        );
       })}
     </div>
   );
 };
 
-const KeyComponent = (props) => {
-  return (
-    <div className="Keyboard-key" id={props.letter}>
-      {props.letter}
-    </div>
-  );
-};
-
-const KeyRowComponent = (props) => {
-  return (
-    <div className="Keyboard-row">
-      {props.keyRow.map((letter, index) => {
-        return <KeyComponent key={index} letter={letter}></KeyComponent>;
-      })}
-    </div>
-  );
-};
-
-const KeyBoardComponent = (props) => {
+const KeyComponent = ({ letter, handleKeyEvent }) => {
   return (
     <div
-      className="Keyboard-grid"
-      onClick={
-        ("click",
-        (e) => {
-          const newGuess = e.target.id;
-          console.log(newGuess);
-          if (newGuess === "") {
-            return;
-          }
-          const updatedWordleGuessList = [...props.wordleGuessList];
-          const rowCoord = props.arrCoords[0];
-          const indexCoord = handleDelete(newGuess, props.arrCoords[1]);
-          const newWordleRow = updatedWordleGuessList[rowCoord];
-          if (indexCoord === 5 && newGuess !== "Delete") {
-            return;
-          }
-          newWordleRow[indexCoord] = setKeyValue(newGuess, indexCoord);
-          const newIndex = setIndexValue(newGuess, indexCoord);
-          const newArrCoords = [...props.arrCoords];
-          newArrCoords[1] = newIndex;
-          props.setArrayCoords(newArrCoords);
-        })
-      }
+      className="Keyboard-key"
+      id={letter}
+      onClick={("click", handleKeyEvent)}
     >
+      {letter}
+    </div>
+  );
+};
+
+const KeyRowComponent = ({ keyRow, handleKeyEvent }) => {
+  return (
+    <div className="Keyboard-row">
+      {keyRow.map((letter, index) => {
+        return (
+          <KeyComponent
+            key={index}
+            letter={letter}
+            handleKeyEvent={handleKeyEvent}
+          ></KeyComponent>
+        );
+      })}
+    </div>
+  );
+};
+
+const KeyBoardComponent = ({ handleKeyEvent }) => {
+  return (
+    <div className="Keyboard-grid">
       {keyBoardArr.map((row, index) => {
-        return <KeyRowComponent key={index} keyRow={row}></KeyRowComponent>;
+        return (
+          <KeyRowComponent
+            key={index}
+            keyRow={row}
+            handleKeyEvent={handleKeyEvent}
+          ></KeyRowComponent>
+        );
       })}
     </div>
   );
@@ -138,16 +164,40 @@ const setKeyValue = (newGuess, indexCoord) => {
 };
 
 const setIndexValue = (newGuess, indexCoord) => {
-  if (indexCoord <= 4 && newGuess === "Delete") {
+  if (newGuess === "Delete") {
+    return indexCoord;
+  }
+  if (newGuess === "Enter") {
+    // Do Something
+    return;
+  }
+  if (indexCoord <= 4) {
+    // return newGuess;
+    return indexCoord + 1;
+  }
+  // return indexCoord + 1;
+  return;
+  /* if (indexCoord <= 4 && newGuess === "Delete") {
     return indexCoord;
   }
   if (indexCoord <= 4 && newGuess !== "Delete" && newGuess !== "Enter") {
     return indexCoord + 1;
   }
-  return indexCoord;
+  return indexCoord; */
 };
 
 const handleDelete = (newGuess, indexToSet) => {
+  /* if (newGuess === "Delete") {
+    return indexCoord;
+  }
+  if (newGuess === "Enter") {
+    // Do Something
+    return;
+  }
+  if (indexCoord <= 4) {
+    return newGuess;
+  }
+  return indexCoord + 1; */
   if (newGuess === "Delete" && indexToSet > 0 && indexToSet <= 5) {
     return indexToSet - 1;
   }
